82c5f1d1797b1e7f8065861416710092b3148f87
Yury Bely <teremo4ek@gmail.com>
2022-05-19 17:30:33 +0300

    complicated screen formed with swift code

--- HomeForSale/Screens/DetailScreen/DetailScreenViewController.swift
+++ HomeForSale/Screens/DetailScreen/DetailScreenViewController.swift
@@ -11,63 +11,114 @@ import UIKit
 final class DetailScreenViewController : UIViewController {
     var viewModel: DetailScreenViewModel!
     
+    private var imageView: UIImageView!
+    private var titleLabel: UILabel!
+    private var descriptionLabel: UILabel!
+    private var livingAreaLabel: UILabel!
+    private var livingAreaTitle: UILabel!
+    private var livingAreaEnd: UILabel!
+    private var loginButton: UIButton!
+    private var nameTextField: UITextField!
     
-    var loginButton: UIButton!
-    var nameTextField: UITextField!
-    var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
         
+        let image = UIImage(systemName: "homekit")
+        imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .cyan
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(imageView)
+        
+        titleLabel = UILabel(frame: .zero)
+        titleLabel.text = viewModel.title
+        titleLabel.font = .boldSystemFont(ofSize: 16)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        
+        descriptionLabel = UILabel(frame: .zero)
+        descriptionLabel.text = viewModel.description
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        
+        livingAreaLabel = UILabel(frame: .zero)
+        livingAreaLabel.text = viewModel.livingAreaLable
+        livingAreaLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(livingAreaLabel)
+        
+        livingAreaTitle = UILabel(frame: .zero)
+        livingAreaTitle.text = viewModel.livingAreaTitle
+        livingAreaTitle.font = .boldSystemFont(ofSize: 16)
+        livingAreaTitle.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(livingAreaTitle)
+        
+        livingAreaEnd = UILabel(frame: .zero)
+        livingAreaEnd.text = "end"
+        livingAreaEnd.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(livingAreaEnd)
+        
         loginButton = UIButton(type: .system)
-        loginButton.setTitle("Login", for: .normal)
+        loginButton.setTitle("OK!", for: .normal)
+        loginButton.backgroundColor = .darkGray
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginButton)
         loginButton.addTarget(self, action: #selector(handleLoginTouchUpInside), for: .touchUpInside)
         
         nameTextField = UITextField(frame: .zero)
-        nameTextField.placeholder = "Login Name"
+        nameTextField.placeholder = "Name"
         nameTextField.borderStyle = .roundedRect
         nameTextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nameTextField)
+
         
-        passwordTextField = UITextField(frame: .zero)
-        passwordTextField.placeholder = "Password"
-        passwordTextField.isSecureTextEntry = true
-        passwordTextField.borderStyle = .roundedRect
-        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(passwordTextField)
         
         constraintsInit()
     }
     
     func constraintsInit() {
         NSLayoutConstraint.activate([
-            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor, constant: 10),
+            imageView.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: 20),
+            imageView.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -20),
+            imageView.heightAnchor.constraint(equalToConstant: 400.0),
             
-            passwordTextField.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -20),
-            passwordTextField.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: 20),
-            passwordTextField.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -20),
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -20),
             
-            nameTextField.bottomAnchor.constraint(equalTo: passwordTextField.topAnchor, constant: -20),
-            nameTextField.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: 20),
-            nameTextField.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -20)
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: 20),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -20),
+            
+            livingAreaTitle.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            livingAreaTitle.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: 20),
+            livingAreaTitle.trailingAnchor.constraint(greaterThanOrEqualTo: livingAreaLabel.leadingAnchor, constant: -10),
+            
+            livingAreaLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            livingAreaLabel.trailingAnchor.constraint(greaterThanOrEqualTo: livingAreaEnd.leadingAnchor, constant: -10),
+            
+            livingAreaEnd.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            livingAreaEnd.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -20),
+            
+            loginButton.topAnchor.constraint(equalTo: livingAreaTitle.bottomAnchor, constant: 20),
+            loginButton.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: 20),
+            loginButton.trailingAnchor.constraint(greaterThanOrEqualTo: nameTextField.leadingAnchor, constant: -10),
+            
+            nameTextField.topAnchor.constraint(equalTo: livingAreaTitle.bottomAnchor, constant: 10),
+            nameTextField.widthAnchor.constraint(equalToConstant: 200.0),
+            nameTextField.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -20),
         ])
     }
     
     @objc func handleLoginTouchUpInside() {
         print("Login has been tapped")
         if nameTextField.isFirstResponder {
-            nameTextField.resignFirstResponder()
+          nameTextField.resignFirstResponder()
         }
-        if passwordTextField.isFirstResponder {
-            passwordTextField.resignFirstResponder()
-        }
-    }
-    
-    
-    
-    
+      }
 }
--- HomeForSale/Screens/DetailScreen/DetailScreenViewModel.swift
+++ HomeForSale/Screens/DetailScreen/DetailScreenViewModel.swift
@@ -9,4 +9,7 @@ import Foundation
 
 final class DetailScreenViewModel {
     let title = "DetailScreenViewModel"
+    let description = "The living room can be furnished according to your own wishes and tastes, here the whole family can gather and enjoy each other's company. From the living room you reach the terrace overlooking the lush courtyard which is located in undisturbed and secluded location."
+    let livingAreaTitle = "Living Area:"
+    let livingAreaLable = "36 m^2"
 }
