e9b67a10d9412c384fbf193286a4ca7e5840255c
Yury Bely <teremo4ek@gmail.com>
2022-05-23 11:36:06 +0300

    use pod in project #1

---
+++ HomeForSale/View/MainController.swift
@@ -0,0 +1,30 @@
+//
+//  MainController.swift
+//  HomeForSale
+//
+//  Created by Yury Bely on 23.05.2022.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
